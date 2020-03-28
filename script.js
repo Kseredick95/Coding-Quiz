573fd63b79b3fac8bd5936e536153415d38d03f5
Karolyn Seredick <kseredick95@gmail.com>
2020-03-28 14:14:50 -0400

    Added rest of questions and score page. Awaiting local storage render. Bug:storing name

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var questionsEl = document.querySelector(".questionnaire");
 var minutesDisplay = document.querySelector("#minutes");
 var secondsDisplay = document.querySelector("#seconds");
 var start = document.querySelector("#startButton");
+var nextBtn = document.querySelector("#nextBtn");
+var questionAsk = document.querySelector("#questionHead");
+var input = document.querySelector("#userInput")
 
 //variables for timer
 var seconds = 60;
@@ -13,6 +16,18 @@ var secondsElapsed = 0;
 //question variables
 var i = 0;
 var score = 0;
+var askQuestion;
+var correctBtn;
+var answerBtn = 0;
+var buttons;
+var highScores = "High Scores: "
+
+//Storage
+var scoreStore = window.localStorage;
+
+
+//next button
+nextBtn = document.createElement("BUTTON").innerText = "Next";
 
 var questions = ["What is a helpful to test your code?", "What does .forEach do?", "Which is NOT an input type?"]
 
@@ -24,22 +39,166 @@ var falseAnswersTwo = ["Confuse you", "Magic", "Replace a while loop"];
 
 var falseAnswersThree = ["var", "boolean", "string"];
 
-//Questions and Answers
-function questionOne() {
+//Highscores
+function highScore() {
+    $("#nextBtn").remove();
+    $("#buttons").empty();
+    $("#questionHead").empty();
 
-    falseAnswersOne.forEach(falseAnswer => {
+    //header
+    questionAsk = $(`<h2>` + highScores + `</h2>`)
+    $("#questionHead").append(questionAsk);
+
+    //create score list
+    var scoreList = document.querySelector("ul");
+    scoreList.innerText = ""
+
+    //Add text input
+    input = document.createElement("INPUT", "class = input");
+    input.setAttribute("type", "text");
+    input.setAttribute("placeholder", "Enter name");
+    document.body.appendChild(input);
+
+    //Submit text
+    var submitBtn = document.createElement("BUTTON");
+    submitBtn.innerHTML = "Submit";
+    $("#buttons").append(submitBtn);
+
+    function renderInput(inputText) {
+        var li = document.createElement("li");
+        li.textContent = inputText + " Score:" + score;
+        scoreList.appendChild(li);
+
+
+        //Fix inputText to state name. Find way to load prior highscores
+        scoreStore.setItem("Name", inputText);
+        scoreStore.setItem("Score", score);
+    }
+
+    $(submitBtn).on("click", function (event) {
+        event.preventDefault();
+
+        var inputText = input.value.trim();
+        input.prepend(inputText);
+        input.value = "";
+
+        renderInput(inputText);
+
+
+    });
+}
+
+
+//Questions and Answers
+function questionThree() {
+    $("#buttons").empty();
+    $("#questionHead").empty();
+
+    questionAsk = $(`<h2>` + questions[2] + `</h2>`)
+    $("#questionHead").append(questionAsk);
+
+
+    falseAnswersThree.forEach(falseAnswer => {
         //Gives each item in falseAnswers a button
-        var answerBtn = $(`<button class= "button" data-number=${falseAnswer}>` + falseAnswer + `</button>`);
+        answerBtn = $(`<button class= "button" data-number=${falseAnswer}>` + falseAnswer + `</button>`);
 
         $("#buttons").append(answerBtn);
     });
 
     //Creates button for correct answer
     var correctBtn = document.createElement("BUTTON");
+    correctBtn.innerHTML = answers[2];
+    $("#buttons").append(correctBtn);
+
+    $(answerBtn).on("click", function () {
+        $("#nextBtn").on("click", function () {
+            highScore();
+        });
+
+    });
+
+    $(correctBtn).on("click", function () {
+        score += 10;
+        $("#nextBtn").on("click", function () {
+            highScore();
+        });
+        console.log(score);
+        return score;
+
+    });
+
+}; //end function
+
+function questionTwo() {
+    $("#buttons").empty();
+    $("#questionHead").empty();
+
+
+    questionAsk = $(`<h2>` + questions[1] + `</h2>`)
+    $("#questionHead").append(questionAsk);
+
+
+    falseAnswersTwo.forEach(falseAnswer => {
+        //Gives each item in falseAnswers a button
+        answerBtn = $(`<button class= "button" data-number=${falseAnswer}>` + falseAnswer + `</button>`);
+
+        $("#buttons").append(answerBtn);
+    });
+
+    //Creates button for correct answer
+    var correctBtn = document.createElement("BUTTON");
+    correctBtn.innerHTML = answers[1];
+    $("#buttons").append(correctBtn);
+
+    $(answerBtn).on("click", function () {
+        $("#nextBtn").on("click", function () {
+            questionThree();
+        });
+
+    });
+
+    $(correctBtn).on("click", function () {
+        score += 10;
+        $("#nextBtn").on("click", function () {
+            questionThree();
+        });
+        return score;
+
+    });
+
+}; //end function
+
+function questionOne() {
+
+    //Print question
+    questionAsk = $(`<h2>` + questions[0] + `</h2>`)
+    $("#questionHead").append(questionAsk);
+
+    falseAnswersOne.forEach(falseAnswer => {
+        //Gives each item in falseAnswers a button
+        answerBtn = $(`<button class= "button" data-number=${falseAnswer}>` + falseAnswer + `</button>`);
+
+        $("#buttons").append(answerBtn);
+    });
+
+    //Creates button for correct answer
+    correctBtn = document.createElement("BUTTON");
     correctBtn.innerHTML = answers[0];
     $("#buttons").append(correctBtn);
 
-    //create click functions with if/else statements for highscores and next question
+    $(answerBtn).on("click", function () {
+        $("#nextBtn").addClass("btn").append(nextBtn).on("click", function () {
+            questionTwo();
+        });
+    });
+
+    $(correctBtn).on("click", function () {
+        score += 10;
+        $("#nextBtn").addClass("btn").append(nextBtn).on("click", function () {
+            questionTwo();
+        });
+        return score;
+    });
 
 }; //end function
 
@@ -82,9 +241,9 @@ function getFormattedSeconds() {
 }
 
 function printTime() {
-
     minutesDisplay.innerHTML = getFormattedMinutes();
     secondsDisplay.textContent = getFormattedSeconds();
+
 }
 
 function startTime() {
